8733eca417c0ee2fe20fe7018c32c4d5f5122981
Shayan Amani <7shayan7@gmail.com>
2024-01-27 22:14:46 -0500

    Declare TypeScript namespace interface definitions specific to Cloudflare

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,8 +2,29 @@
 // for information about these interfaces
 // and what to do when importing types
 declare namespace App {
-	// interface Locals {}
+	interface Locals {
+	}
+
 	// interface PageData {}
 	// interface Error {}
-	// interface Platform {}
+
+	// Include support for environment variables.
+	// The `env` object, containing KV namespaces and other storage objects,
+	// is passed to SvelteKit via the platform property along with context
+	// and caches, meaning you can access it in hooks and endpoints.
+	interface Platform {
+		env: {
+			COUNTER: DurableObjectNamespace;
+		};
+		context: {
+			waitUntil(promise: Promise<any>): void;
+		};
+		caches: CacheStorage & { default: Cache };
+	}
+
+	interface Session {
+	}
+
+	interface Stuff {
+	}
 }
