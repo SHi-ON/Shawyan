114e91e1a32668fab5cf9ee74676dcd012be4d22
Shayan Amani <7shayan7@gmail.com>
2024-01-27 23:27:49 -0500

    Reformat the entire project by Prettier

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,29 +2,26 @@
 // for information about these interfaces
 // and what to do when importing types
 declare namespace App {
-	interface Locals {
-	}
+  interface Locals {}
 
-	// interface PageData {}
-	// interface Error {}
+  // interface PageData {}
+  // interface Error {}
 
-	// Include support for environment variables.
-	// The `env` object, containing KV namespaces and other storage objects,
-	// is passed to SvelteKit via the platform property along with context
-	// and caches, meaning you can access it in hooks and endpoints.
-	interface Platform {
-		env: {
-			COUNTER: DurableObjectNamespace;
-		};
-		context: {
-			waitUntil(promise: Promise<any>): void;
-		};
-		caches: CacheStorage & { default: Cache };
-	}
+  // Include support for environment variables.
+  // The `env` object, containing KV namespaces and other storage objects,
+  // is passed to SvelteKit via the platform property along with context
+  // and caches, meaning you can access it in hooks and endpoints.
+  interface Platform {
+    env: {
+      COUNTER: DurableObjectNamespace;
+    };
+    context: {
+      waitUntil(promise: Promise<any>): void;
+    };
+    caches: CacheStorage & { default: Cache };
+  }
 
-	interface Session {
-	}
+  interface Session {}
 
-	interface Stuff {
-	}
+  interface Stuff {}
 }
