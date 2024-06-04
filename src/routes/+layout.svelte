64b2abb3867aacf03c7b4b3ad41ac9417ab086b4
SHi-ON <10985542+SHi-ON@users.noreply.github.com>
2024-06-03 23:24:34 -0400

    Reformat using Prettier

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import '../app.postcss';
   import { AppShell, AppBar } from '@skeletonlabs/skeleton';
-  import {Github} from 'lucide'
 </script>
 
 <!-- App Shell -->
@@ -18,31 +17,55 @@
           href="https://x.shawyan.com/github"
           target="_blank"
           rel="noreferrer">
-          <img height="28" width="28" alt='GitHub icon' src="https://cdn.simpleicons.org/github/white" />
+          <img
+            height="28"
+            width="28"
+            alt="GitHub icon"
+            src="https://cdn.simpleicons.org/github/white" />
         </a>
         <a
           class="variant-glass-primary btn"
           href="https://x.shawyan.com/google-scholar"
           target="_blank"
           rel="noreferrer">
-          <img height="28" width="28" alt='Google Scholar icon' src="https://cdn.simpleicons.org/googlescholar/white" />
+          <img
+            height="28"
+            width="28"
+            alt="Google Scholar icon"
+            src="https://cdn.simpleicons.org/googlescholar/white" />
         </a>
         <a
           class="variant-glass-primary btn"
           href="https://x.shawyan.com/linkedin"
           target="_blank"
           rel="noreferrer">
-          <img height="28" width="28" alt='LinkedIn icon' src="https://cdn.simpleicons.org/linkedin/white" />
+          <img
+            height="28"
+            width="28"
+            alt="LinkedIn icon"
+            src="https://cdn.simpleicons.org/linkedin/white" />
         </a>
-        <a class="variant-glass-primary btn" href="https://x.shawyan.com/x" target="_blank" rel="noreferrer">
-          <img height="28" width="28" alt='X icon' src="https://cdn.simpleicons.org/x/white" />
+        <a
+          class="variant-glass-primary btn"
+          href="https://x.shawyan.com/x"
+          target="_blank"
+          rel="noreferrer">
+          <img
+            height="28"
+            width="28"
+            alt="X icon"
+            src="https://cdn.simpleicons.org/x/white" />
         </a>
         <a
           class="variant-glass-primary btn"
           href="https://x.shawyan.com/stack-overflow"
           target="_blank"
           rel="noreferrer">
-          <img height="28" width="28" alt='Stack Overflow icon' src="https://cdn.simpleicons.org/stackoverflow/white" />
+          <img
+            height="28"
+            width="28"
+            alt="Stack Overflow icon"
+            src="https://cdn.simpleicons.org/stackoverflow/white" />
         </a>
       </svelte:fragment>
     </AppBar>
